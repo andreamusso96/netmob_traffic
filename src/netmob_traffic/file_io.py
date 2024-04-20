3e6fc22fe025cf79c7a3966178bd0882601ab49e
andreamusso96 <andreamusso96@gmail.com>
2024-04-20 15:44:32 +0200

    Update paths to data

--- src/netmob_traffic/file_io.py
+++ src/netmob_traffic/file_io.py
@@ -18,7 +18,7 @@ def initialize_data_directory(folder: str):
 def get_mobile_traffic_data_file_path(traffic_type: TrafficType, city: City, service: Service, day: date):
     assert data_dir is not None, 'Data directory not initialized, call initialize_data_directory before using this function'
     day_str = day.strftime('%Y%m%d')
-    path = f'{data_dir}/tile/{city.value}/{service.value}/{day_str}/'
+    path = f'{data_dir}/traffic/{city.value}/{service.value}/{day_str}/'
     file_name = f'{city.value}_{service.value}_{day_str}_{traffic_type.value}.txt'
     file_path = path + file_name
     return file_path
@@ -26,7 +26,7 @@ def get_mobile_traffic_data_file_path(traffic_type: TrafficType, city: City, ser
 
 def get_geo_data_file_path(city: City) -> str:
     assert data_dir is not None, 'Data directory not initialized, call initialize_data_directory before using this function'
-    return f'{data_dir}/{city.value}.geojson'
+    return f'{data_dir}/tile_geo/{city.value}.geojson'
 
 
 def save_mobile_traffic_data_by_tile_service_time(city: City, data: xr.DataArray, folder_path: str):
